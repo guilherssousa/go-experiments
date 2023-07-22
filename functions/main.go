99262025fd28f972ec19508a6fc27d450ec61f09
Guilherme Sousa <guilherssousa@gmail.com>
2023-07-22 10:35:07 -0300

    Introdução a funções (Aula 8)

--- functions/main.go
+++ functions/main.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+
+	return soma, subtracao
+}
+
 func main() {
-	return 1
+	soma := somar(10, 20)
+	fmt.Println(soma)
+
+	var f = func() string {
+		fmt.Println("função f")
+		return "hello world"
+	}
+
+	f()
+
+  resultadoSoma, _ := calculosMatematicos(125, 125)
+  fmt.Println(resultadoSoma)
 }
