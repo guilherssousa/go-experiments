f2a8aabbcec096668598035589e62d66a299a598
Guilherme Sousa <guilherssousa@gmail.com>
2023-07-26 00:31:49 -0300

    Renderizando HTML Templates (Aula 52)

--- html/main.go
+++ html/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"log"
-	"net/http"
+  "log"
+  "net/http"
+  "html/template"
 )
 
+type usuario struct {
+  Nome string
+  Email string 
+}
+
+var templates *template.Template
+
 func main() {
+  templates = template.Must(template.ParseGlob("*.html"))
 
   http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("hELLO FUTURE"))
+    u:= usuario{
+      "Guilherme",
+      "gui@guisousa.com",
+    }
+    templates.ExecuteTemplate(w, "index.html",u) 
   })
 
   log.Fatal(http.ListenAndServe(":3000", nil))
