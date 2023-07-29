37125c1e5a46ecfd9bfa56c3c14a0d9145424b56
Guilherme Sousa <guilherssousa@gmail.com>
2023-07-29 17:29:21 -0300

    add logs for error cases

--- crud/server/server.go
+++ crud/server/server.go
@@ -6,6 +6,7 @@ import (
   "fmt"
   "io/ioutil"
   "strconv"
+  "log"
 
   "net/http"
 
@@ -23,6 +24,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
   requestBody, err := ioutil.ReadAll(r.Body)  
 
   if err != nil {
+    log.Print(err)
     w.Write([]byte("Falha ao ler o corpo da requisição."))
     return
   }
@@ -30,12 +32,14 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
   var user user 
 
   if err = json.Unmarshal(requestBody, &user); err != nil {
+    log.Print(err)
     w.Write([]byte("Erro ao converter o corpo da requisição"))    
     return
   }
 
   db, err := database.Connect()
   if err != nil {
+    log.Print(err)
     w.Write([]byte("Erro ao se conectar com o banco de dados."))
     return
   }
@@ -44,32 +48,29 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
   // INSERT INTO usuarios (nome, email) values ("nome", "email")
 
   // PREPARE STATEMENT
-  statement, err :=  db.Prepare("INSERT INTO usuarios (nome, email) values (?, ?)")
+  statement, err :=  db.Prepare("INSERT INTO usuarios (nome, email) values ($1, $2)")
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao criar o statement!"))
     return
   }
 
-  insert, err := statement.Exec(user.Nome, user.Email)
+  _, err = statement.Exec(user.Nome, user.Email)
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao executar o statement!"))
     return
   }
 
-  insertId, err := insert.LastInsertId()
-  if err != nil {
-    w.Write([]byte("Erro ao obter o ID inserido!"))
-    return
-  }
-
   w.WriteHeader(http.StatusCreated)
-  w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", insertId)))
+  w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso!")))
 }
 
 // SearchUsers list all Users
 func ListUsers(w http.ResponseWriter, r *http.Request) {
   db, err := database.Connect()
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao conectar com o banco de dados!"))
     return
   }
@@ -77,6 +78,7 @@ func ListUsers(w http.ResponseWriter, r *http.Request) {
 
   rows, err := db.Query("SELECT * FROM usuarios")
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao buscar os usuários"))
     return
   }
@@ -88,6 +90,7 @@ func ListUsers(w http.ResponseWriter, r *http.Request) {
     var user user
 
     if err := rows.Scan(&user.ID, &user.Nome, &user.Email); err != nil {
+      log.Println(err)
       w.Write([]byte("Erro ao escanear o usuário."))
       return
     }
@@ -97,6 +100,7 @@ func ListUsers(w http.ResponseWriter, r *http.Request) {
 
   w.WriteHeader(http.StatusOK)
   if err := json.NewEncoder(w).Encode(users); err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao converter os usuários para JSON."))
     return
   }
@@ -107,19 +111,22 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 
   ID, err := strconv.ParseUint(params["id"], 10, 32)
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao converter os dados da requisição."))
     return
   }
 
   db, err := database.Connect()
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao conectar com o banco de dados!"))
     return
   }
   defer db.Close()
 
-  row, err := db.Query("SELECT * FROM usuarios WHERE id = ?", ID)
+  row, err := db.Query("SELECT * FROM usuarios WHERE id = $1", ID)
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao buscar o usuário")) 
     return 
   }
@@ -127,6 +134,7 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
   var user user
   if row.Next() {
     if err := row.Scan(&user.ID, &user.Nome, &user.Email); err != nil {
+      log.Println(err)
       w.Write([]byte("Erro ao mapear dados para o usuário"))
       return
     }
@@ -134,6 +142,7 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 
   w.WriteHeader(http.StatusOK)
   if err := json.NewEncoder(w).Encode(user); err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao converter o usuário para JSON!"))
     return
   } 
@@ -144,37 +153,43 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 
   ID, err := strconv.ParseUint(params["id"], 10, 32)
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao converter os dados da requisição."))
     return
   }
 
   requestBody, err := ioutil.ReadAll(r.Body)
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Falha ao ler o corpo da requisição."))
     return
   }
 
   var user user
   if err = json.Unmarshal(requestBody, &user); err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao converter o corpo da requisição"))    
     return
   }
-  
+
   db, err := database.Connect();
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao abrir conexão com o banco de dados!"))
     return
   }
   defer db.Close()
 
-  statement, err := db.Prepare("UPDATE usuarios SET nome = ?, email = ? WHERE id = ?")
+  statement, err := db.Prepare("UPDATE usuarios SET nome = $1, email = $2 WHERE id = $3")
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao criar o statement!"))
     return
   }
   defer statement.Close()
 
   if _, err := statement.Exec(user.Nome, user.Email, ID); err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao atualizar o usuário"))
     return
   }
@@ -184,31 +199,35 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
   params := mux.Vars(r)
-  
+
   ID, err := strconv.ParseUint(params["id"], 10, 32)
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao converter os dados da requisição."))
     return
   }
-  
+
   db, err := database.Connect()
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao conectar com o banco de dados."))
     return
   }
   defer db.Close()
 
-  statement, err := db.Prepare("DELETE FROM usuarios WHERE id = ?")
+  statement, err := db.Prepare("DELETE FROM usuarios WHERE id = $1")
   if err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao criar o statement!"))
     return
   }
   defer statement.Close()
 
   if _, err := statement.Exec(ID); err != nil {
+    log.Println(err)
     w.Write([]byte("Erro ao deletar o usuário."))
     return
   }
-  
+
   w.WriteHeader(http.StatusNoContent)
 }
