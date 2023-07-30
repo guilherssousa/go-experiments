dec9a089f39c2cb406ac93033ebe0b2bd8f4d91b
Guilherme Sousa <guilherssousa@gmail.com>
2023-07-30 15:59:06 -0300

    add support to markdown parsing and syntax highlighting

--- crud/public/message.js
+++ crud/public/message.js
@@ -2,11 +2,32 @@ const messageTitle = document.querySelector("#message-title");
 const messageDescription = document.querySelector("#message-description");
 const contentElement = document.querySelector("#content");
 
+const { Marked } = globalThis.marked;
+const { markedHighlight } = globalThis.markedHighlight;
+
 async function main() {
   console.log("fetching message data...");
 
+  const highlighter = await shiki.getHighlighter({
+    theme: "monokai",
+    langs: ["js", "sql"],
+  });
+
+  const marked = new Marked(
+    markedHighlight({
+      langPrefix: "code-block language-",
+      highlight(code, lang) {
+        return highlighter.codeToHtml(code, { lang });
+      },
+    }),
+    {
+      mangle: false,
+      headerIds: false,
+    }
+  );
+
   const { searchParams } = new URL(window.location.href);
-  const id = searchParams.get("id");  
+  const id = searchParams.get("id");
 
   if (!id) {
     alert("Nao e possivel achar a mensagem.");
@@ -20,8 +41,11 @@ async function main() {
   messageDescription.innerHTML = `
     por ${json.nome} \<<a href="mailto:${json.email}">${json.email}</a>\>
   `;
-
-  contentElement.innerHTML = marked.parse(json.content); 
+  
+  console.log("parsing and highlighting code....");
+  console.time("parser");
+  contentElement.innerHTML = marked.parse(json.content);
+  console.timeEnd("parser");
 }
 
 main();
